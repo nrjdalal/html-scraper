3b60993315d04bb00cb3c6ef428f6e4d0b2c5fd2
Neeraj Dalal <admin@nrjdalal.com>
2024-09-22 15:01:35 +0530

    feat: added ip switching

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,45 @@
+import * as AWS from "@aws-sdk/client-lambda"
 import express from "express"
 import serverless from "serverless-http"
 
 const app = express()
+
+app.get("/switch", async (req, res) => {
+  const start = performance.now()
+
+  const lambdaClient = new AWS.LambdaClient({
+    region: "ap-south-1",
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    },
+  })
+
+  const existingConfig = await lambdaClient.send(
+    new AWS.GetFunctionConfigurationCommand({
+      FunctionName: process.env.FUNCTION_NAME,
+    }),
+  )
+
+  await lambdaClient.send(
+    new AWS.UpdateFunctionConfigurationCommand({
+      FunctionName: process.env.FUNCTION_NAME,
+      Environment: {
+        Variables: {
+          RUN: `${Math.random()}`,
+          ...existingConfig.Environment.Variables,
+        },
+      },
+    }),
+  )
+
+  await new Promise((resolve) => setTimeout(resolve, 5000))
+
+  const response = await fetch("https://api.apify.com/v2/browser-info")
+  const data = await response.json()
+
+  res.json({ ...data, time: (performance.now() - start).toFixed(0) })
+})
 
 app.get("/", async (req, res) => {
   const start = performance.now()
--- sst.config.ts
+++ sst.config.ts
@@ -9,11 +9,6 @@ export default $config({
       name: "html-scraper",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
-      providers: {
-        aws: {
-          region: "ap-south-1",
-        },
-      },
     }
   },
 
@@ -21,22 +16,36 @@ export default $config({
     const schema = z
       .object({
         API_KEY: z.string().min(8),
+        SST_AWS_ACCESS_KEY_ID: z.string(),
+        SST_AWS_SECRET_ACCESS_KEY: z.string(),
+        PULUMI_NODEJS_PROJECT: z.string(),
         PULUMI_NODEJS_STACK: z.enum(["dev"]),
       })
       .parse(process.env)
 
-    const lambda = new sst.aws.Function("html-scraper", {
-      handler: "src/index.handler",
-      memory: "512 MB",
-      runtime: "nodejs20.x",
-      url: true,
-      environment: {
-        API_KEY: schema.API_KEY,
+    const apSouth1 = new sst.aws.Function(
+      "html-scraper",
+      {
+        handler: "src/index.handler",
+        memory: "512 MB",
+        runtime: "nodejs20.x",
+        url: true,
+        environment: {
+          API_KEY: schema.API_KEY,
+          ACCESS_KEY_ID: schema.SST_AWS_ACCESS_KEY_ID,
+          SECRET_ACCESS_KEY: schema.SST_AWS_SECRET_ACCESS_KEY,
+          FUNCTION_NAME: `${schema.PULUMI_NODEJS_PROJECT}-${schema.PULUMI_NODEJS_STACK}-${schema.PULUMI_NODEJS_PROJECT.replace("-", "")}Function`,
+          FUNCTION_REGION: "ap-south-1",
+        },
       },
-    })
+      {
+        provider: new aws.Provider("ap-south-1", { region: "ap-south-1" }),
+      },
+    )
 
     return {
-      URL: pulumi.interpolate`${lambda.url}?key=${schema.API_KEY}&search=https://api.apify.com/v2/browser-info`,
+      Console: "https://console.aws.amazon.com/lambda",
+      URL: pulumi.interpolate`${apSouth1.url}?key=${schema.API_KEY}&search=https://api.apify.com/v2/browser-info`,
     }
   },
 })
